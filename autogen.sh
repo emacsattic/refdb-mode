b731bfe04a015cb36a3f66f4759fe824cdf57320
mhoenicka <mhoenicka@471ad660-1a11-0410-a8c5-95cef8d96d26>
2006-07-09 18:12:32 +0000

    minor updates
    git-svn-id: svn://svn.code.sf.net/p/refdb/code/elisp/trunk@77 471ad660-1a11-0410-a8c5-95cef8d96d26

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# HACK HACK HACK! The autotools want the .texi file but we generate
+# it from the DocBook sources at build time. We create a mock
+# file and remove it again after the autotools have done their magic
 echo "@setfilename refdb-mode.info" >refdb-mode.texi
 aclocal
 automake --add-missing
